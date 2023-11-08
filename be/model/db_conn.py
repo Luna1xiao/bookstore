bd026ad439b603bf468f6f11564b500f02858664
Turrrdidae <112929926+Turrrdidae@users.noreply.github.com>
2023-11-08 14:19:20 +0800

    更新为连接到MongoDB数据库；修改为使用MongoDB实现user功能

--- be/model/db_conn.py
+++ be/model/db_conn.py
@@ -3,35 +3,19 @@ from be.model import store
 
 class DBConn:
     def __init__(self):
-        self.conn = store.get_db_conn()
+        self.db = store.get_db_conn()
 
     def user_id_exist(self, user_id):
-        cursor = self.conn.execute(
-            "SELECT user_id FROM user WHERE user_id = ?;", (user_id,)
-        )
-        row = cursor.fetchone()
-        if row is None:
-            return False
-        else:
-            return True
+        query = {"user_id": user_id}
+        result = self.db["user"].find_one(query)
+        return result is not None
 
     def book_id_exist(self, store_id, book_id):
-        cursor = self.conn.execute(
-            "SELECT book_id FROM store WHERE store_id = ? AND book_id = ?;",
-            (store_id, book_id),
-        )
-        row = cursor.fetchone()
-        if row is None:
-            return False
-        else:
-            return True
+        query = {"store_id": store_id, "book.book_id": book_id}
+        result = self.db["store"].find_one(query)
+        return result is not None
 
     def store_id_exist(self, store_id):
-        cursor = self.conn.execute(
-            "SELECT store_id FROM user_store WHERE store_id = ?;", (store_id,)
-        )
-        row = cursor.fetchone()
-        if row is None:
-            return False
-        else:
-            return True
+        query = {"store_id": store_id}
+        result = self.db["store"].find_one(query)
+        return result is not None
